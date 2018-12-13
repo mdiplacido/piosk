e08c9b2dd6bfbb2d90dca8e6c221b90af799d34a
Marco DiPlacido <marcodiplacido@Marcos-MBP.home>
2018-12-13 17:03:22 -0500

    updating nav, bringing in fontawesome

--- kiosk-browser-client/src/components/navigation.tsx
+++ kiosk-browser-client/src/components/navigation.tsx
@@ -1,4 +1,10 @@
+import { faChevronLeft, faChevronRight, faPause } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
+
+const back = <FontAwesomeIcon icon={faChevronLeft} />
+const pause = <FontAwesomeIcon icon={faPause} />
+const forward = <FontAwesomeIcon icon={faChevronRight} />
 
 export interface INavigation {
     back: () => void;
@@ -9,9 +15,9 @@ export interface INavigation {
 export const Navigation = (props: INavigation) => {
     return (
         <div className="navigation-container">
-            <button onClick={props.back}>Back</button>
-            <button onClick={props.pause}>Pause</button>
-            <button onClick={props.forward}>Forward</button>
+            <button onClick={props.back}>{back}</button>
+            <button onClick={props.pause}>{pause}</button>
+            <button onClick={props.forward}>{forward}</button>
         </div>
     );
 }
