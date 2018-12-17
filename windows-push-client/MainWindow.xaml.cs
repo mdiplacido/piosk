c856700857ec36ca509ba665b014a681c126184d
Marco <marcodiplacido@gmail.com>
2018-12-17 00:06:34 -0500

    simple screen cap working.  it will not handle issues where another window is over the ViewPort or if the viewport is off the screen.

--- windows-push-client/MainWindow.xaml.cs
+++ windows-push-client/MainWindow.xaml.cs
@@ -1,20 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace windows_push_client
+﻿namespace windows_push_client
 {
+    using System;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -28,6 +18,36 @@ namespace windows_push_client
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Viewport.Navigate(this.Location.Text);
+        }
+
+        private void CaptureScreen_Click(object sender, RoutedEventArgs e)
+        {
+            var topLeft = this.TopLeft();
+            TakeScreenshot((int)Math.Ceiling(topLeft.X), (int)Math.Ceiling(topLeft.Y), (int)Math.Ceiling(this.Viewport.ActualWidth), (int)Math.Ceiling(this.Viewport.ActualHeight));
+        }
+
+        private void TakeScreenshot(int StartX, int StartY, int Width, int Height)
+        {
+            // Bitmap in right size
+            Bitmap screenshot = new Bitmap(Width, Height);
+            Graphics g = Graphics.FromImage(screenshot);
+            // snip wanted area
+            g.CopyFromScreen(StartX, StartY, 0, 0, new System.Drawing.Size(Width, Height), CopyPixelOperation.SourceCopy);
+
+            const string screenCapFile = @"c:\temp\screencap\browsercap.png";
+
+            // save uncompressed bitmap to disk
+            using (System.IO.FileStream fs = System.IO.File.Open(screenCapFile, System.IO.FileMode.OpenOrCreate))
+            {
+                screenshot.Save(fs, System.Drawing.Imaging.ImageFormat.Bmp);
+            }
+
+            Process.Start(screenCapFile);
+        }
+
+        private System.Windows.Point TopLeft()
+        {
+            return this.Viewport.PointToScreen(new System.Windows.Point(0, 0));
         }
     }
 }
