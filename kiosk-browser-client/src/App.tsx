0afebbfca8e97274e77abba118fa4d7543510595
Marco DiPlacido <marcodiplacido@Marcos-MacBook-Pro.local>
2018-12-27 02:01:15 -0500

    now able to push a single image down over the socket

--- kiosk-browser-client/src/App.tsx
+++ kiosk-browser-client/src/App.tsx
@@ -1,17 +1,49 @@
-import * as React from 'react';
 import './App.css';
+
+import * as React from 'react';
+import Sockette from 'sockette';
+
 import { Navigation } from './components/navigation';
 
+interface IState {
+  connected: boolean;
+  image?: string;
+  ws?: Sockette;
+}
+
 class App extends React.Component {
+  public state: IState = { connected: false };
+
+  public componentDidMount() {
+    const ws = new Sockette("ws://localhost:8080", {
+      onmessage: msg => this.setState({ image: msg.data }),
+      onopen: () => this.setState({ connected: true }),
+    });
+
+    this.setState({ ws });
+  }
+
   public render() {
-    return (
-      <div className="App">
-        <div className="jumble-tron">
-          todo: here is where the images will display
+    const connected = !!this.state.connected;
+
+    if (!connected) {
+      return (
+        <div>Connecting...</div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <div className="jumble-tron">
+            {
+              this.state.image ?
+                <img src={`data:image/png;base64,${this.state.image}`} /> :
+                <div>No images to show</div>
+            }
+          </div>
+          <Navigation back={this.onBack} forward={this.onForward} pause={this.onPause} />
         </div>
-        <Navigation back={this.onBack} forward={this.onForward} pause={this.onPause} />
-      </div>
-    );
+      );
+    }
   }
 
   private onBack = () => {
