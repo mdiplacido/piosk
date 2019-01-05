3a7f74a78abe0044b623a3d9dbd4236e4349a7cd
Marco <marcodiplacido@gmail.com>
2019-01-05 13:42:41 -0500

    changing LogViewer to use RichTextBox

--- windows-push-client/LogView.xaml.cs
+++ windows-push-client/LogView.xaml.cs
@@ -3,7 +3,6 @@
 namespace windows_push_client
 {
     using System;
-    using System.Text;
     using System.Windows.Controls;
     using windows_push_client.Services;
 
@@ -12,8 +11,6 @@ namespace windows_push_client
     /// </summary>
     public partial class LogView : UserControl, IObserver<string>
     {
-        private readonly StringBuilder sb = new StringBuilder();
-
         public LogView(LoggingService logging)
         {
             InitializeComponent();
@@ -27,19 +24,14 @@ namespace windows_push_client
 
         public void OnError(Exception error)
         {
-            this.sb.AppendLine(string.Format("Stream error: {0}", error.ToString()));
-            this.UpdateTextArea();            
+            this.LogTextBox.AppendText(string.Format("Stream error: {0}{1}", error.ToString(), Environment.NewLine));
+            this.LogTextBox.ScrollToEnd();
         }
 
         public void OnNext(string value)
         {
-            this.sb.AppendLine(value);
-            this.UpdateTextArea();
-        }
-
-        private void UpdateTextArea()
-        {
-            this.LogTextBox.Text = this.sb.ToString();
+            this.LogTextBox.AppendText(value + Environment.NewLine);
+            this.LogTextBox.ScrollToEnd();
         }
     }
 }
