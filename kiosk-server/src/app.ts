fcc864d02b95beee627b2fcf23803d8fb507d3da
mdiplacido <mdiplacido@users.noreply.github.com>
2019-01-09 12:11:54 -0500

    Refactor server to app class (#1)
    * cleaning up mess
    
    * cleaning up logging

--- kiosk-server/src/app.ts
+++ kiosk-server/src/app.ts
@@ -1,13 +1,130 @@
-import { Logger } from "./logging/logger";
+import * as chokidar from 'chokidar';
+import * as WebSocket from 'ws';
+import {
+    catchError,
+    filter,
+    mergeMap,
+    takeUntil,
+    tap
+    } from 'rxjs/operators';
+import { Config } from './config/config';
+import { empty as observableEmpty, ReplaySubject, Subject } from 'rxjs';
+import { existsSync, mkdirpSync, Stats } from 'fs-extra';
+import { Logger } from './logging/logger';
+import { ReadFileStream } from './io/read-file-stream';
+
+interface PathStatsPair {
+    readonly path: string;
+    readonly stats: Stats;
+}
+
+const PATH_FILTER_PREDICATE =
+    (pathStats: PathStatsPair) => !!pathStats && pathStats.path.toLowerCase().endsWith(".png");
 
 export class App {
     private readonly logger: Logger;
+    private readonly fileChangeSource = new ReplaySubject<PathStatsPair>(10);
+    private readonly wss = new WebSocket.Server({ port: this.config.port });
+    private fileSystemWatcher: chokidar.FSWatcher;
 
-    constructor(logger: Logger) {
+    constructor(private readonly config: Config, logger: Logger) {
         this.logger = logger.createScopedLogger("App", true /* increment depth */);
     }
 
     public run(): void {
-        this.logger.info("Running...")
+        this.ensurePickupDirectory();
+        this.setupFileSystemWatcher();
+        this.listenOnWebSocket();
+        this.startReaper();
+        this.logger.info("WebSocket Server Running...");
+    }
+
+    private listenOnWebSocket(): void {
+        this.wss.on('connection', (ws, req) => {
+            this.logger.info(`Got new client connection from remote: ${req.connection.remoteAddress}`);
+            const wsClosed = new Subject();
+
+            ws.on('message', message => {
+                this.logger.info(`received: ${message} from client`);
+            });
+
+            ws.on('close', () => {
+                this.logger.info(`closing connection with client: ${req.connection.remoteAddress}`);
+                wsClosed.next();
+                wsClosed.complete();
+            });
+
+            // here we setup a subscription to the FS change source and assuming the client is still connected
+            // eg. "wsClosed" has not emitted then we read the file and push it back down to the client.
+            this.fileChangeSource
+                .pipe(
+                    takeUntil(wsClosed),
+                    filter(PATH_FILTER_PREDICATE),
+                    tap(({ path }) => this.logger.verbose(`attempting to read file ${path}`)),
+                    mergeMap(pathStats =>
+                        ReadFileStream.fromPath(pathStats.path).pipe(
+                            catchError(error => {
+                                // we have to catch errors so we keep the stream alive.
+                                this.logger.error(`got retriable error: ${error}`);
+                                return observableEmpty();
+                            }))
+                    ),
+                    // if we get this far we are un-watching that means if a file reappears for some reason we will not see
+                    // it from the watcher again.  don't see why this would be necessary.
+                    tap(({ path }) => {
+                        this.logger.verbose(`un-watching path: ${path}`)
+
+                        try {
+                            this.fileSystemWatcher.unwatch(path)
+                        } catch (error) {
+                            this.logger.error(`failed to un-watch the path ${path}`);
+                        }
+                    })
+                )
+                // not it is possible for files to be replayed that are no longer on disk, this will be handled and logged
+                // in the error block here
+                .subscribe(({ data }) => {
+                    ws.send(data.toString('base64'), error => error && this.logger.error(`got error ${error} sending to client`));
+                }, err => this.logger.error(`got terminal Error! - ${err}`));
+        });
+    }
+
+    private startReaper(): void {
+        this.logger.info("Starting reaper...");
+    }
+
+    private setupFileSystemWatcher(): void {
+        this.fileSystemWatcher = chokidar.watch(this.config.pickupDirectory, {
+            alwaysStat: true,
+            // wait for writes to have been stable for 2 seconds, check for changes every 100ms.
+            // i guess the thinking is that the latest you would know is after 2100 ms.
+            awaitWriteFinish: {
+                pollInterval: 100,
+                stabilityThreshold: 2000,
+            }
+        });
+
+        // watch for new files or changing files
+        // NOTE: we cannot guarantee the order of the files, it's up to the client
+        // to deal with sorting what it receives and dumping files it does not care
+        // about.
+        this.fileSystemWatcher
+            .on("add", (path, stats: Stats) => {
+                this.logger.verbose(`got to watcher:add path is '${path}'`)
+                this.fileChangeSource.next({ path, stats });
+            })
+            .on("change", (path, stats: Stats) => {
+                this.logger.verbose(`got to watcher:change path is '${path}'`)
+                this.fileChangeSource.next({ path, stats });
+            });
+    }
+
+    private ensurePickupDirectory(): void {
+        // ensure that pickup directory exists.
+        if (!existsSync(this.config.pickupDirectory + "/")) {
+            this.logger.warn(`pickup directory ${this.config.pickupDirectory} does not exist, creating...`);
+            // make recursive as the parent folder may not exist.
+            mkdirpSync(this.config.pickupDirectory);
+        }
     }
 }
--- kiosk-server/src/index.ts
+++ kiosk-server/src/index.ts
@@ -1,27 +1,13 @@
-// tslint:disable:no-submodule-imports
-// tslint:disable:no-expression-statement
-import * as chokidar from "chokidar";
-import { existsSync, mkdirpSync, Stats } from "fs-extra";
-import { empty as observableEmpty, ReplaySubject, Subject } from "rxjs";
-import { catchError, filter, mergeMap, takeUntil, tap } from "rxjs/operators";
-import * as WebSocket from "ws";
-import { argv } from "yargs";
-
-import { App } from "./app";
-import { Config } from "./config/config";
-import { ReadFileStream } from "./io/read-file-stream";
-import { ConsoleLogger } from "./logging/console-logger";
-import { dumpSettings } from "./utility/dump-settings";
+import { App } from './app';
+import { argv } from 'yargs';
+import { Config } from './config/config';
+import { ConsoleLogger } from './logging/console-logger';
+import { dumpSettings } from './utility/dump-settings';
 
 // we read from a jailed location on the Pi, clients that push to the Pi push over SFTP and the SSHD jails these
 // users this location on disk.
 const DEFAULT_PICKUP_DIRECTORY = "/var/jail/data/piosk_pickup";
 const DEFAULT_PORT = 8081;
-
-interface PathStatsPair {
-    readonly path: string;
-    readonly stats: Stats;
-}
 
 const config: Config = {
     pickupDirectory: (argv.pickup || "").replace(/[\\\/]+$/, '') || DEFAULT_PICKUP_DIRECTORY,
@@ -32,89 +18,5 @@ const config: Config = {
 const logger = new ConsoleLogger("Main")
 dumpSettings(config, logger);
 
-const app = new App(logger);
+const app = new App(config, logger);
 app.run();
-
-
-// ensure that pickup directory exists.
-if (!existsSync(config.pickupDirectory + "/")) {
-    logger.warn(`pickup directory ${config.pickupDirectory} does not exist, creating...`);
-    // make recursive as the parent folder may not exist.
-    mkdirpSync(config.pickupDirectory);
-}
-
-// we will replay the last n file events
-const fileChangeSource = new ReplaySubject<PathStatsPair>(10);
-
-const wss = new WebSocket.Server({ port: config.port });
-
-const PATH_FILTER_PREDICATE =
-    (pathStats: PathStatsPair) => !!pathStats && pathStats.path.toLowerCase().endsWith(".png");
-
-// start watcher for new and changing files.
-const watcher = chokidar.watch(config.pickupDirectory, {
-    alwaysStat: true,
-    // wait for writes to have been stable for 2 seconds, check for changes every 100ms.
-    // i guess the thinking is that the latest you would know is after 2100 ms.
-    awaitWriteFinish: {
-        pollInterval: 100,
-        stabilityThreshold: 2000,
-    }
-});
-
-// TODO: we need a reaper to remove files that are older than some threshold.
-
-// watch for new files or changing files
-// NOTE: we cannot guarantee the order of the files, it's up to the client
-// to deal with sorting what it receives and dumping files it does not care
-// about.
-watcher
-    .on("add", (path, stats: Stats) => {
-        logger.verbose(`got to watcher:add path is '${path}'`)
-        fileChangeSource.next({ path, stats });
-    })
-    .on("change", (path, stats: Stats) => {
-        logger.verbose(`got to watcher:change path is '${path}'`)
-        fileChangeSource.next({ path, stats });
-    });
-
-wss.on('connection', (ws, req) => {
-    logger.info(`Got new client connection from remote: ${req.connection.remoteAddress}`);
-    const wsClosed = new Subject();
-
-    ws.on('message', message => {
-        logger.info('received: %s', message);
-    });
-
-    ws.on('close', () => {
-        wsClosed.next();
-        wsClosed.complete();
-    });
-
-    // tslint:disable-next-line:no-expression-statement
-    fileChangeSource
-        .pipe(
-            takeUntil(wsClosed),
-            filter(PATH_FILTER_PREDICATE),
-            tap(({ path }) => logger.verbose(`attempting to read file ${path}`)),
-            mergeMap(pathStats =>
-                ReadFileStream.fromPath(pathStats.path).pipe(
-                    catchError(error => {
-                        // we have to catch errors so we keep the stream alive.
-                        logger.error(`got retriable error: ${error}`);
-                        return observableEmpty();
-                    }))
-            ),
-            // if we get this far we are unwatching that means if a file reappears for some reason we will not see
-            // it from the watcher again.  don't see why this would be necessary.
-            tap(({ path }) => {
-                logger.verbose(`unwatching path: ${path}`)
-                watcher.unwatch(path)
-            })
-        )
-        // not it is possible for files to be replayed that are no longer on disk, this will be handled and logged
-        // in the error block here
-        .subscribe(({ data }) => ws.send(data.toString('base64')), err => logger.error(`got terminal Error! - ${err}`));
-});
-
-logger.info("running...");
