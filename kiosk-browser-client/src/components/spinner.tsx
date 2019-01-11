23453e8cdba5f64d2a9f5f558150058e85550b0d
mdiplacido <mdiplacido@users.noreply.github.com>
2019-01-11 16:07:46 -0500

    adding spinner (#4)

---
+++ kiosk-browser-client/src/components/spinner.tsx
@@ -0,0 +1,23 @@
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as React from "react";
+
+import './spinner.css';
+
+export interface ISpinnerProps {
+    spin?: boolean;
+    text?: string;
+}
+
+export const Spinner = (props: ISpinnerProps) => {
+    return (
+        <div className="spinner-box">
+            <div className="center">
+                <FontAwesomeIcon icon={faSpinner} size="5x" spin={props.spin} />
+            </div>
+            <div className="text center" hidden={!props.text}>
+                {props.text}
+            </div>
+        </div>
+    );
+}
