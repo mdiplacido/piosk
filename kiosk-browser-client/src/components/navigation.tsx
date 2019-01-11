232ca7ba79df8e632c708599196b8027cc595734
Marco DiPlacido <marcodiplacido@Marcos-MacBook-Pro.local>
2019-01-11 00:44:35 -0500

    styling nav to look slicker

--- kiosk-browser-client/src/components/navigation.tsx
+++ kiosk-browser-client/src/components/navigation.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import { faChevronLeft, faChevronRight, faPause } from '@fortawesome/free-solid-svg-icons';
+import { faPause, faStepBackward, faStepForward } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './navigation.css';
 
-const back = <FontAwesomeIcon icon={faChevronLeft} />
-const pause = <FontAwesomeIcon icon={faPause} />
-const forward = <FontAwesomeIcon icon={faChevronRight} />
+const back = <FontAwesomeIcon icon={faStepBackward} size="2x" pull="left" />
+const pause = <FontAwesomeIcon icon={faPause} size="2x" className="fl" />
+const forward = <FontAwesomeIcon icon={faStepForward} size="2x" pull="right" />
 
 export interface INavigation {
     back: () => void;
@@ -17,9 +17,9 @@ export const Navigation = (props: INavigation) => {
     return (
         <div className="navigation-center-container">
             <div className="navigation-container">
-                <button onClick={props.back}>{back}</button>
-                <button onClick={props.pause}>{pause}</button>
-                <button onClick={props.forward}>{forward}</button>
+                <button id="back-button" onClick={props.back}>{back}</button>
+                <button id="pause-button" onClick={props.pause}>{pause}</button>
+                <button id="forward-button" onClick={props.forward}>{forward}</button>
             </div>
         </div>
     );
