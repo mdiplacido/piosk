aa50f88e47aec926bc6f5886e73bb5e5614841cc
JJ Wang <junwang@microsoft.com>
2019-01-11 14:55:20 -0500

    css adjustment

--- kiosk-browser-client/src/components/navigation.tsx
+++ kiosk-browser-client/src/components/navigation.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { faChevronLeft, faChevronRight, faPause, faQrcode } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faChevronRight, faPause, faQrcode, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './navigation.css';
 
@@ -7,6 +7,7 @@ const back = <FontAwesomeIcon icon={faChevronLeft} />
 const pause = <FontAwesomeIcon icon={faPause} />
 const forward = <FontAwesomeIcon icon={faChevronRight} />
 const qrcode = <FontAwesomeIcon icon={faQrcode} />
+const close = <FontAwesomeIcon icon={faTimesCircle} />
 
 export interface INavigation {
   openNav: boolean;
@@ -16,30 +17,25 @@ export interface INavigation {
   pause: () => void;
 }
 
-
-
 export const Navigation = (props: INavigation)=> {
     const navigationClass = props.openNav? ' open' : ' close'; 
-
     function handleOnClick(event:any){
         event.preventDefault();
-        console.log(event.currentTarget);
         props.toggleNav();
     }
 
-    console.log('props: '+props.openNav+'; class: '+navigationClass); 
     return (
         <div 
             className={"navigation-center-container" + navigationClass}>
             <div className="navigation-container">
+                <span className='close-button' onClick={handleOnClick}>{close}</span>
+
                 <button onClick={props.back}>{back}</button>
                 <button onClick={props.pause}>{pause}</button>
                 <button onClick={props.forward}>{forward}</button>
 
                 {/* category ddl fits here  */}
                 <button>{qrcode}</button>
-
-                <span onClick={handleOnClick}>x</span>
             </div>
         </div>
     );
