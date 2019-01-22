577afd503fe7382a464f0affc9b1d7423630accf
Marco DiPlacido <marcodiplacido@Marcos-MacBook-Pro.local>
2019-01-21 22:59:38 -0500

    giving more control to the user for slide show and pause behavior

--- kiosk-browser-client/src/components/navigation.tsx
+++ kiosk-browser-client/src/components/navigation.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import {
+    faCheckCircle,
     faChevronLeft,
     faChevronRight,
+    faCircle,
     faPause,
     faQrcode,
     faTimesCircle
-    } from '@fortawesome/free-solid-svg-icons';
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './navigation.scss';
 
+const circle = <FontAwesomeIcon icon={faCircle} />
+const checkedCircle = <FontAwesomeIcon icon={faCheckCircle} />
 const back = <FontAwesomeIcon icon={faChevronLeft} />
 const pause = (isPaused: boolean) => <FontAwesomeIcon icon={faPause} spin={isPaused} />
 const forward = <FontAwesomeIcon icon={faChevronRight} />
@@ -21,11 +25,13 @@ export interface INavigation {
     disablePrev: boolean;
     disableQR: boolean;
     isPaused: boolean;
+    slideshowEnabled: boolean;
     closeNav: () => void;
     back: () => void;
     forward: () => void;
     pause: () => void;
     toggleQR: () => void;
+    toggleSlideshow: () => void;
 }
 
 export const Navigation = (props: INavigation) => {
@@ -48,6 +54,10 @@ export const Navigation = (props: INavigation) => {
 
                 {/* category ddl fits here  */}
                 <button onClick={props.toggleQR} disabled={props.disableQR}>{qrcode}</button>
+
+                <span className={props.slideshowEnabled ? 'slideshow-enabled' : 'slideshow-disabled'} onClick={props.toggleSlideshow}>
+                    {props.slideshowEnabled ? checkedCircle : circle} slideshow
+                </span>
             </div>
         </div>
     );
