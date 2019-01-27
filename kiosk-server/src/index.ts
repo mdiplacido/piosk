d80626cce87b4b324fecf333b341b460f41727f0
Marco DiPlacido <marcodiplacido@Marcos-MBP.fios-router.home>
2019-01-27 01:25:29 -0500

    disk logging now works and we log unexpected exits

--- kiosk-server/src/index.ts
+++ kiosk-server/src/index.ts
@@ -1,9 +1,13 @@
-import * as _ from 'lodash';
-import { App } from './app';
-import { argv } from 'yargs';
-import { Config } from './config/config';
-import { ConsoleLogger } from './logging/console-logger';
-import { dumpSettings } from './utility/dump-settings';
+import * as _ from "lodash";
+import * as log4js from "log4js";
+import { argv } from "yargs";
+
+import { App } from "./app";
+import { Config } from "./config/config";
+import { AggregateLogger } from "./logging/aggregate-logger";
+import { ConsoleLogger } from "./logging/console-logger";
+import { FileLogger } from "./logging/file-logger";
+import { dumpSettings } from "./utility/dump-settings";
 
 // we read from a jailed location on the Pi, clients that push to the Pi push over SFTP and the SSHD jails these
 // users this location on disk.
@@ -19,7 +23,25 @@ const config: Config = {
     pickupQuotaMb: +argv.pickupQuotaMb || 100,
 };
 
-const logger = new ConsoleLogger("Main")
+const fileLoggerImp = log4js
+    .configure({
+        appenders: { piosk: { type: 'file', filename: 'piosk.log' } },
+        categories: { default: { appenders: ['piosk'], level: 'ALL' } }
+    })
+    .getLogger();
+
+const fileLogger = new FileLogger(fileLoggerImp, "Main");
+const consoleLogger = new ConsoleLogger("Main");
+
+const logger = new AggregateLogger(consoleLogger, fileLogger);
+
+process.on('uncaughtException', (err) => {
+    // intentionally logging in one log statement.  seems that with multiple log statements
+    // the file does not not get the second log statement flushed to disk.
+    logger.error(`uncaughtException: ${err.message} stack: ${err.stack}`);
+    process.exit(1)
+})
+
 dumpSettings(config, logger);
 
 const app = new App(config, logger);
