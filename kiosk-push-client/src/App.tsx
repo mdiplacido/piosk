c02d13ddc280e67361eccfb7d3fdb1ce8f0d4066
Marco DiPlacido <marcodiplacido@Marcos-MacBook-Pro.local>
2019-02-16 14:33:53 -0500

    initial check-in.  everything works well, but i need to upgrade react and all other dependencies to latest versions

--- kiosk-push-client/src/App.tsx
+++ kiosk-push-client/src/App.tsx
@@ -1,28 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-class App extends Component {
+export class App extends React.Component<undefined, undefined> {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>Welcome to React with Typescript!</h2>
       </div>
     );
   }
 }
-
-export default App;
