2a7b4ad123279a3f22b4d5b6b18c83c6a657e6a0
mdiplacido <mdiplacido@users.noreply.github.com>
2019-02-17 20:07:01 -0500

    Electron (#11)
    * basic react boiler plate with typescript and installed electron
    
    * basic rendering working
    
    * initial check-in.  everything works well, but i need to upgrade react and all other dependencies to latest versions
    
    * updating react to latest version
    
    * Revert "updating react to latest version"
    
    This reverts commit ef77fb26e1d8b70883d7970c8c384dd410e4e3a9.
    
    * explicitly including electron
    
    * poc sort of working.  one site not behaving well
    
    * POC is done.  now need to port over the experience along with the I/O piece of sending to the Disk or PI over SFTP
    
    * latest version of react is running.  need to fix typing issues next.

---
+++ kiosk-push-client/README.md
@@ -0,0 +1,11 @@
+# PIOSK Push Client
+
+This Application was bootstrapped with React + TypeScript starter template: https://electronforge.io/templates
+
+using commands:
+
+    > npm install -g electron-forge
+    > electron-forge init kiosk-push-client --template=react-typescript
+    > cd kiosk-push-client
+    > electron-forge start
+
---
+++ kiosk-push-client/src/App.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { remote, BrowserWindow } from 'electron';
+import * as fs from 'fs';
+
+interface State {
+  url: string;
+}
+
+export class App extends React.Component<undefined, State> {
+  private renderWindow: BrowserWindow;
+
+  constructor(props?: any, context?: any) {
+    super(props, context);
+    this.state = {
+      url: 'http://www.clocktab.com/'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>POC for screen capture</h2>
+
+        current url: {this.state.url}
+        <br />
+        <button onClick={this.screenshot}>Screenshot</button>
+        <br />
+        <button onClick={this.openWindow}>Open new window</button>
+        <br />
+        <button onClick={this.refresh}>Refresh</button>
+        <br />
+        <button onClick={this.maximize}>Maximize</button>
+        <br />
+        <button onClick={this.loadNewUrl}>Change url</button> to: {this.state.url}
+        <br />
+        <input type='text' onChange={this.handleOnUrlChange} />
+      </div>
+    );
+  }
+
+  handleOnUrlChange = (event: any) => {
+    const target = event.target as HTMLInputElement;
+    this.setState({ url: target.value });
+  }
+
+  loadNewUrl = () => {
+    this.renderWindow.loadURL(this.state.url);
+  }
+
+  maximize = () => {
+    this.renderWindow.setFullScreen(true);
+  }
+
+  refresh = () => {
+    // using the refresh is not super reliable, it's much better just to reload the browser
+    // location
+    this.renderWindow.loadURL(this.state.url);
+  }
+
+  openWindow = () => {
+    this.renderWindow = new remote.BrowserWindow({
+      webPreferences: {
+        nodeIntegration: false,
+        webSecurity: false /* probably should make this an option per capture */
+      }
+    });
+    this.renderWindow.loadURL(this.state.url);
+
+    // todo: attaching the dev tools should be optional and may not be available depending on
+    // the build.
+    this.renderWindow.webContents.openDevTools();
+  }
+
+  screenshot = () => {
+    this.renderWindow.capturePage(img =>
+      fs.writeFile('/var/jail/data/piosk_pickup/test.png', img.toPNG(),
+        () => {
+          // callback, no-op for now
+        }));
+  }
+}
