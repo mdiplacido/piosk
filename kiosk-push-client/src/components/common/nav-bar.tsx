796fe55a643ff9e1be3bd627d5b3c4d16196c829
Marco DiPlacido <marcodiplacido@Marcos-MacBook-Pro.local>
2019-02-19 21:55:15 -0500

    cleaning nav logic

--- kiosk-push-client/src/components/common/nav-bar.tsx
+++ kiosk-push-client/src/components/common/nav-bar.tsx
@@ -20,6 +20,8 @@ const NavBar = (props: NavBarProps) => {
     const { classes } = props;
     const { history } = props;
 
+    const navigateTo = (path: string) => () => { history.push(path); };
+
     return (
         <div>
             <AppBar position="static">
@@ -27,10 +29,10 @@ const NavBar = (props: NavBarProps) => {
                     <Typography variant="h5" color="inherit" className={classes.grow}>
                         Piosk Push Client
                     </Typography>
-                    <Button color="inherit" onClick={() => history.push("/")}>Controller</Button>
-                    <Button color="inherit" onClick={() => history.push("/settings")}>Settings</Button>
-                    <Button color="inherit" onClick={() => history.push("/test")}>Test</Button>
-                    <Button color="inherit" onClick={() => history.push("/logs")}>Logs</Button>
+                    <Button color="inherit" onClick={navigateTo("/")}>Controller</Button>
+                    <Button color="inherit" onClick={navigateTo("/settings")}>Settings</Button>
+                    <Button color="inherit" onClick={navigateTo("/test")}>Test</Button>
+                    <Button color="inherit" onClick={navigateTo("/logs")}>Logs</Button>
                 </Toolbar>
             </AppBar>
         </div>
