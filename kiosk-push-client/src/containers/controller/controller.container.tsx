0bf5b92def1dbd1408b143b32b576331fe4ca543
Marco DiPlacido <marcodiplacido@Marcos-MBP.fios-router.home>
2019-03-11 00:28:15 -0400

    adding add tooltip to bottom of controller container

--- kiosk-push-client/src/containers/controller/controller.container.tsx
+++ kiosk-push-client/src/containers/controller/controller.container.tsx
@@ -1,17 +1,43 @@
-import { withStyles } from "@material-ui/core";
 import * as React from "react";
-import { useEffect, useState } from "react";
-
+import AddIcon from "@material-ui/icons/Add";
 import CaptureCard from "../../components/capture/card";
+import containerStyles from "../../components/common/styles";
 import PageContainer from "../../components/common/page-container";
-import containerStyles, { ContainerStyleProps } from "../../components/common/styles";
-import { ConfigConsumerProps, withConfig } from "../../providers/config/config.provider";
+import {
+    ConfigConsumerProps,
+    withConfig
+    } from "../../providers/config/config.provider";
+import {
+    createStyles,
+    Fab,
+    Theme,
+    Tooltip,
+    WithStyles,
+    withStyles
+    } from "@material-ui/core";
+import {
+    useEffect,
+    useState
+    } from "react";
 
-export interface ControllerContainerProps extends ContainerStyleProps, ConfigConsumerProps {
+export interface ControllerContainerProps extends ConfigConsumerProps, WithStyles<typeof styles> {
 }
+
+const styles = (theme: Theme) => createStyles({
+    fab: {
+        margin: theme.spacing.unit * 2,
+    },
+    absolute: {
+        position: "absolute",
+        bottom: theme.spacing.unit * 3,
+        right: theme.spacing.unit * 3,
+    },
+    ...containerStyles(theme)
+});
 
 const ControllerContainer = (props: ControllerContainerProps) => {
     const [now, setNow] = useState(new Date());
+    const { classes } = props;
 
     useEffect(() => {
         // tick now every 1 second.
@@ -33,8 +59,13 @@ const ControllerContainer = (props: ControllerContainerProps) => {
                         </React.Fragment>
                     )
             }
+            <Tooltip title="Add" aria-label="Add" onClick={() => alert("Todo add modal")}>
+                <Fab color="secondary" className={classes.absolute}>
+                    <AddIcon />
+                </Fab>
+            </Tooltip>
         </PageContainer>
     );
 };
 
-export default withConfig(withStyles(containerStyles)(ControllerContainer));
+export default withConfig(withStyles(styles)(ControllerContainer));
